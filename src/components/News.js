5fa9b6760f7d81a681fa201c273398603281ed13
mariyashah007 <mariyashah260798@gmail.com>
2022-10-21 22:20:12 +0530

    infinitescroll fixed

--- src/components/News.js
+++ src/components/News.js
@@ -8,9 +8,9 @@ import InfiniteScroll from "react-infinite-scroll-component";
 const News = (props) =>{
 
   const [articles, setArticles] = useState([])
-  const [loading, setLoading] = useState([true])
-  const [page, setPage] = useState([1])
-  const [totalResults, setTotalResults] = useState([0])
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState(0)
 
  
 
